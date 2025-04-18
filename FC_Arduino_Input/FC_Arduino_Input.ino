918a07fe592a9131d35bf021a58602641532d288
Kokuko3 <mcdaniel.121@wright.edu>
2025-04-18 13:53:54 -0400

    Update FC_Arduino_Input.ino

--- FC_Arduino_Input/FC_Arduino_Input.ino
+++ FC_Arduino_Input/FC_Arduino_Input.ino
@@ -2,163 +2,129 @@
 #include <AS5600.h>
 #include <Wire.h>                   
 
-#define THROTTLE_IN_PIN A0
+#define THROTTLE_IN_PIN D5
 #define PITCH_IN_PIN A1
 #define ROLL_IN_PIN A2
 #define OUTPUT_PIN D4
+
 #define ONESHOT_MIN 125    
 #define ONESHOT_MAX 250  
-AMS_5600 ams5600;
-float pitch;            // 0.0 to 1.0
-float phase;            // 0.0 to TWO_PI
-double angle;           // Current rotor angle
-double adjustedAngle;           // Current rotor angle
-float roll;
-byte throttle;
-byte power;
-byte Duty;
-double lastAngle;       // for RPM tracking - see below
-int revolutions;        // for RPM tracking
-double rpms;
-#define REPORT_INTERVAL 1000
-unsigned long lastReport = 0; // Time (in ms) of last report
-unsigned long currentTime;    // Current time in ms
-unsigned currentReport;       // Number of current report
-// Report RPMs every RPM_INTERVAL reports
-#define RPM_INTERVAL 10
 
+AMS_5600 ams5600;
+
+float pitch = 1.0;         // Static for now
+float phase = 2.75f;
+double angle;
+double adjustedAngle;
+
+volatile unsigned long throttleRiseTime = 0;
+volatile int throttle = 150;  // µs
+volatile bool newThrottle = false;
+
+int Duty;
 
 void setup() {
   delay(10000);
   Serial.begin(115200);
   Serial.println("Initializing...");
 
-  // set up Hall effect Sensor
   Wire.begin();
   ams5600setup();
 
   pinMode(OUTPUT_PIN, OUTPUT);
   pinMode(THROTTLE_IN_PIN, INPUT);
-  pinMode(PITCH_IN_PIN, INPUT);
-  pinMode(ROLL_IN_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(THROTTLE_IN_PIN), throttleISR, CHANGE);
 
-  Serial.println("ESC Calibration...");
+  Serial.println("Calibrating: zero signal...");
+  sendOneShotPulse(ONESHOT_MIN);
+  delay(8000);
 
-  Serial.println("Sending MAX throttle...");
-  sendOneShotPulse(250);
-  delay(8000);  // Wait for ESC to detect max throttle
+  // Raise the "stick" from zero to PWM_MAX
+  Serial.println("Calibrating: raising stick...");
+  for (byte i = ONESHOT_MIN; i <= ONESHOT_MAX; i++){
+    sendOneShotPulse(i);
+    delay(100);
+  }
 
-  Serial.println("Sending MIN throttle...");
-  sendOneShotPulse(125);
-  delay(8000);  // Wait for ESC to confirm calibration
+  Serial.println("Calibrating: max signal...");
+  sendOneShotPulse(ONESHOT_MAX);
+  delay(8000);
 
-  Serial.println("ESC calibration complete.");
-  pitch = 0.0f;
-  phase = 2.75f;
+  // Lower the "stick" back to min
+  Serial.println("Calibrating: lowering stick...");
+  for (byte i = ONESHOT_MAX; i >= ONESHOT_MIN; i--){
+    sendOneShotPulse(i);
+    delay(100);
+  }
+
+  // Hold "min" for 10 seconds
+  Serial.println("Calibrating: minimum value...");
+  sendOneShotPulse(ONESHOT_MIN);
+  delay(10000);
+  Serial.println("ESC calibration complete.\n");
 }
 
-/*******************************************************
-/* Function: sendOneShotPulse
-/* In: The pulse width of the one shot signal to send 
-/* Out: Writes a digital signal 
-/* Description: Sends out a PWM signal within the
-/* pulse width of OneShot125
-/*******************************************************/
+void loop() {
+  if (newThrottle) {
+    noInterrupts();       
+    int localThrottle = throttle;
+    newThrottle = false;
+    interrupts();
+
+    // Calculate motor angle
+    angle = convertRawAngleToRadians(ams5600.getRawAngle());
+    adjustedAngle = angle + phase;
+    if (adjustedAngle > TWO_PI) adjustedAngle -= TWO_PI;
+
+    // Map and modulate
+    int safeThrottle = constrain(localThrottle, ONESHOT_MIN, ONESHOT_MAX);
+    float throttle_scale = map(safeThrottle, ONESHOT_MIN, ONESHOT_MAX, 1, 100);
+    float sineMod = pitch * 0.5f;  // 50% modulation range
+    float mod = 1.0f + sineMod * sin(adjustedAngle);
+    float power = throttle_scale * mod;
+    Duty = constrain(map(power, 0, 200, ONESHOT_MIN, ONESHOT_MAX), ONESHOT_MIN, ONESHOT_MAX);
+
+    // Output pulse
+    sendOneShotPulse(Duty);
+    Serial.println(Duty);
+  }
+}
+
+void throttleISR() {
+  if (digitalRead(THROTTLE_IN_PIN)) {
+    throttleRiseTime = micros();
+  } else {
+    unsigned long fallTime = micros();
+    unsigned int pulse = fallTime - throttleRiseTime;
+    if (pulse >= 100 && pulse <= 300) {
+      throttle = pulse;
+      newThrottle = true;
+    }
+  }
+}
+
 void sendOneShotPulse(int pulseWidth) {
   digitalWrite(OUTPUT_PIN, HIGH);
   delayMicroseconds(pulseWidth);
   digitalWrite(OUTPUT_PIN, LOW);
 }
 
-/*******************************************************
-/* Function: convertRawAngleToRadians
-/* In: angle data from AMS_5600::getRawAngle
-/* Out: human readable radians as float
-/* Description: takes the raw angle and calculates
-/* float value in radians.
-/*******************************************************/
-float convertRawAngleToRadians(word newAngle){
-  // Raw angle is 0-4095 = 0.001533981 of a radian
-  float retVal = newAngle * 0.001533981;
-  return retVal;
+float convertRawAngleToRadians(word newAngle) {
+  return newAngle * 0.001533981;
 }
 
-/*******************************************************
-/* Function: convertRawAngleToDegrees
-/* In: angle data from AMS_5600::getRawAngle
-/* Out: human readable degrees as float
-/* Description: takes the raw angle and calculates
-/* float value in degrees.
-/*******************************************************/
-float convertRawAngleToDegrees(word newAngle)
-{
-  // Raw data reports 0 - 4095 segments, which is 0.087890625 of a degree
-  float retVal = newAngle * 0.087890625;
-  return retVal;
-}
-
-/*******************************************************
-/* Function: ams5600setup
-/* Description: Sets up the AMS5600 sensor 
-/*******************************************************/
-void ams5600setup(){
-  if(ams5600.detectMagnet() == 0 ){
-    while(1){
-        if(ams5600.detectMagnet() == 1 ){
-            Serial.print("Current Magnitude: ");
-            Serial.println(ams5600.getMagnitude());
-            break;
-        }
-        else{
-            Serial.println("Can not detect magnet");
-        }
+void ams5600setup() {
+  if (ams5600.detectMagnet() == 0) {
+    while (1) {
+      if (ams5600.detectMagnet() == 1) {
+        Serial.print("Current Magnitude: ");
+        Serial.println(ams5600.getMagnitude());
+        break;
+      } else {
+        Serial.println("Cannot detect magnet");
         delay(1000);
+      }
     }
-  }
-}
-
-
-void loop() {
-  throttle = pulseIn(THROTTLE_IN_PIN, HIGH, 50000); // Read pulse width in µs
-  float throttle_scale = map(throttle, ONESHOT_MIN, ONESHOT_MAX, 1, 100);
-
-  pitch = pulseIn(PITCH_IN_PIN, HIGH, 50000); // Read pulse width in µs
-  // roll = pulseIn(ROLL_IN_PIN, HIGH, 50000); // Read pulse width in µs
-
-  // pitch = 2*(pitch/2000)-1;
-  pitch = 0.5;
-  // roll = 2*(roll/2000)-1;
-
-  angle = convertRawAngleToRadians(ams5600.getRawAngle());
-  if ((lastAngle > angle) && ((angle - lastAngle) < -0.01)) revolutions++;
-  adjustedAngle = angle + phase;
-  if (adjustedAngle > TWO_PI){
-    adjustedAngle -= TWO_PI;
-  }
-
-  power = throttle_scale + (pitch * sin(adjustedAngle) * throttle_scale);
-  Duty = map(power, 0, 200, ONESHOT_MIN, ONESHOT_MAX);
-  // Duty = throttle + (pitch * cos(motorRads) * throttle) + (roll * sin(motorRads) * throttle);
-
-  // Serial.println("Angle: " + String(angle) + "  Throttle: " + String(throttle) + "  Pitch: " + String(pitch) + "  Power: " + String(power) + "  Duty: " + String(Duty));
-  Serial.println(Duty);
-  if (throttle < 127) {
-    sendOneShotPulse(0);
-  } else {
-    sendOneShotPulse(Duty);
-  }
-
-  // Report load cell readings every REPORT_INTERVAL ms
-  // Report RPMs every RPM_INTERVAL reports, or (RPM_INTERVAL * REPORT_INTERVAL) ms
-  if (currentTime - lastReport >= REPORT_INTERVAL){
-    if (currentReport % RPM_INTERVAL == 0) {
-      rpms = (double) revolutions * (1000.0 / ((double) REPORT_INTERVAL * (double) RPM_INTERVAL)) * 60;
-      Serial.print("\t" + String(rpms, 2));
-      revolutions = 0;
-    }
-    Serial.println();
-    lastReport = currentTime;
-    currentReport++;
   }
 }
 
